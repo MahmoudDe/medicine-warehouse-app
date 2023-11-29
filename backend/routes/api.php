e5890849822aed71034b6f0766b4cab4743c51cd
Yaman-Th <Tahhan152@gmail.com>
2023-11-29 18:11:13 +0300

    Add Comments in api page

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MedicineController;
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,28 @@ require __DIR__.'/auth.php';
 |
 */
 
+// Medicine Routes
+
+// Return All Medicines from Database 
 Route::get('/medicines', [MedicineController::class, 'index']);
+// To Create New Medicine :: Used By Admin  
 Route::post('/medicines', [MedicineController::class, 'store']);
+// To Edit Medicine :: Used By Admin
 Route::put('/medicines/{medicine}', [MedicineController::class, 'update']);
+// To Delete Medicine :: Used By Admin
 Route::delete('/medicines/{medicine}', [MedicineController::class, 'destroy']);
+// Show By Categories (Filtering)
+
+// Show By one (Medicine Card) 
+
+
+
+// User Routes 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
     return $request->user();
 });
 
+
+
+// Order Routes
--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -5,6 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('login',[AuthController::class,'login']);
-Route::post('register',[AuthController::class,'register']);
-Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
